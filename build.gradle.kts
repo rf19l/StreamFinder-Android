e680e771c474720a2f530a72c2b582d970d5ad97
Ryan Foster <ryan.jfoster94@gmail.com>
2023-02-04 08:06:15 -0500

    Update gradle version for new ide
    Signed-off-by: Ryan Foster <ryan.jfoster94@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("org.jetbrains.kotlin.android") version "1.7.20" apply false
-    id("com.android.application") version "8.0.0-alpha11" apply false
-    id("com.android.library") version "8.0.0-alpha11" apply false
+    id("com.android.application") version "8.1.0-alpha02" apply false
+    id("com.android.library") version "8.1.0-alpha02" apply false
     id("io.realm.kotlin") version "1.4.0" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.7.20" apply false
     kotlin("kapt") version "1.7.20"
